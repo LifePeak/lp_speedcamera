93ab367c1a81f8d09bef2022bc3c85d84e313e92
LvDenu <d2041827@gmail.com>
2022-09-15 20:01:49 +0200

    Added Locales

---
+++ locales/de.lua
@@ -0,0 +1,12 @@
+Locales['de'] = {
+    ['SpeedShoot'] = '%s: Du wurdest mit %d kmH zu schnell geblitzt!',
+
+    --okokBilling
+    ['BillingName'] = 'Blitzer',
+    ["SocietyName"] = "Polizei",
+    ["BillingReason"] = "Blitzer",
+    ["BillingNote"] = "Du wurdest geblitzt.",
+
+        --Job
+    ["IsntPolice"]  = "You are not a cop"
+}
---
+++ locales/en.lua
@@ -0,0 +1,12 @@
+Locales['en'] = {
+    ['SpeedShoot'] = '%s: You drove %d km/H faster than allowed',
+
+    --okokBilling
+    ['BillingName'] = 'Speedcamera',
+    ["SocietyName"] = "Police",
+    ["BillingReason"] = "Speedcamera",
+    ["BillingNote"] = "You got flashed",
+
+    --Job
+    ["IsntPolice"]  = "You are not a cop"
+}
